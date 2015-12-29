bbe6accd4599b208be5aa4e27fcd4a5414ea1390
Michael Brazier <mike.brazier@gmail.com>
2015-12-29 16:17:40 +0000

    First commit

---
+++ nought/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace nought
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.txtBox_main = new System.Windows.Forms.TextBox();
+            this.button_begin = new System.Windows.Forms.Button();
+            this.button_rematch = new System.Windows.Forms.Button();
+            this.lbl_o_score = new System.Windows.Forms.Label();
+            this.lbl_x_score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(28, 80);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(121, 80);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(212, 80);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 62);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(28, 166);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 62);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(121, 166);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 62);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(212, 166);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 62);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "-";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button7.Location = new System.Drawing.Point(28, 247);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(67, 62);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "-";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button8.Location = new System.Drawing.Point(121, 247);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(67, 62);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.button9.Location = new System.Drawing.Point(212, 247);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(67, 62);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "-";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // txtBox_main
+            // 
+            this.txtBox_main.Font = new System.Drawing.Font("Helvetica Neue", 18F, System.Drawing.FontStyle.Bold);
+            this.txtBox_main.Location = new System.Drawing.Point(28, 20);
+            this.txtBox_main.Name = "txtBox_main";
+            this.txtBox_main.Size = new System.Drawing.Size(250, 43);
+            this.txtBox_main.TabIndex = 10;
+            this.txtBox_main.Text = "---->";
+            this.txtBox_main.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button_begin
+            // 
+            this.button_begin.Font = new System.Drawing.Font("Helvetica Neue", 8.999999F, System.Drawing.FontStyle.Bold);
+            this.button_begin.Location = new System.Drawing.Point(299, 20);
+            this.button_begin.Name = "button_begin";
+            this.button_begin.Size = new System.Drawing.Size(85, 122);
+            this.button_begin.TabIndex = 11;
+            this.button_begin.Text = " Begin /Reset";
+            this.button_begin.UseVisualStyleBackColor = true;
+            this.button_begin.Click += new System.EventHandler(this.button_begin_Click);
+            // 
+            // button_rematch
+            // 
+            this.button_rematch.Font = new System.Drawing.Font("Helvetica Neue", 8.999999F, System.Drawing.FontStyle.Bold);
+            this.button_rematch.Location = new System.Drawing.Point(299, 247);
+            this.button_rematch.Name = "button_rematch";
+            this.button_rematch.Size = new System.Drawing.Size(85, 62);
+            this.button_rematch.TabIndex = 12;
+            this.button_rematch.Text = "rematch";
+            this.button_rematch.UseVisualStyleBackColor = true;
+            this.button_rematch.Click += new System.EventHandler(this.button_rematch_Click);
+            // 
+            // lbl_o_score
+            // 
+            this.lbl_o_score.AutoSize = true;
+            this.lbl_o_score.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.lbl_o_score.Location = new System.Drawing.Point(296, 166);
+            this.lbl_o_score.Name = "lbl_o_score";
+            this.lbl_o_score.Size = new System.Drawing.Size(72, 16);
+            this.lbl_o_score.TabIndex = 13;
+            this.lbl_o_score.Text = "score o: 0";
+            // 
+            // lbl_x_score
+            // 
+            this.lbl_x_score.AutoSize = true;
+            this.lbl_x_score.Font = new System.Drawing.Font("Helvetica Neue", 7.8F, System.Drawing.FontStyle.Bold);
+            this.lbl_x_score.Location = new System.Drawing.Point(296, 211);
+            this.lbl_x_score.Name = "lbl_x_score";
+            this.lbl_x_score.Size = new System.Drawing.Size(72, 16);
+            this.lbl_x_score.TabIndex = 14;
+            this.lbl_x_score.Text = "score x: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 336);
+            this.Controls.Add(this.lbl_x_score);
+            this.Controls.Add(this.lbl_o_score);
+            this.Controls.Add(this.button_rematch);
+            this.Controls.Add(this.button_begin);
+            this.Controls.Add(this.txtBox_main);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox txtBox_main;
+        private System.Windows.Forms.Button button_begin;
+        private System.Windows.Forms.Button button_rematch;
+        private System.Windows.Forms.Label lbl_o_score;
+        private System.Windows.Forms.Label lbl_x_score;
+    }
+}
+
---
+++ nought/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nought
+{
+    public partial class Form1 : Form
+    {
+
+        string _flag = "o";             // flag for player turn
+        bool play = false;              // decides if buttons should be active or disabled
+        bool begin = false;             // Begin has been pressed
+        int o_score = 0;                
+        int x_score = 0;                // player scores
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            button_begin.Select();
+            button_begin.Focus();               // sets focus to Begin button
+            
+        }
+
+        string set()                // returns either x or o depending on whoes turn it is and then inverts _flag
+        {
+            switch (_flag)
+            {
+                case "o" :
+                    _flag = "x";
+                    return "o";    
+                case "x" :
+                    _flag = "o";
+                    return "x";
+            }
+            return "";
+        }
+
+
+        void check(string btn_str)              // checks to see if a winning scenario has been achieved
+        {
+            if ((btn_str != "-") & (play))
+            {
+                if ((button1.Text == btn_str & button2.Text == btn_str & button3.Text == btn_str) | (button1.Text == btn_str & button4.Text == btn_str & button7.Text == btn_str) | (button1.Text == btn_str & button5.Text == btn_str & button9.Text == btn_str) | (button2.Text == btn_str & button5.Text == btn_str & button8.Text == btn_str) | (button3.Text == btn_str & button6.Text == btn_str & button9.Text == btn_str) | (button4.Text == btn_str & button5.Text == btn_str & button6.Text == btn_str) | (button7.Text == btn_str & button8.Text == btn_str & button9.Text == btn_str) | (button1.Text == btn_str & button4.Text == btn_str & button7.Text == btn_str) | (button1.Text == btn_str & button5.Text == btn_str & button9.Text == btn_str) | (button2.Text == btn_str & button5.Text == btn_str & button8.Text == btn_str) | (button3.Text == btn_str & button6.Text == btn_str & button9.Text == btn_str) | (button4.Text == btn_str & button5.Text == btn_str & button6.Text == btn_str) | (button7.Text == btn_str & button5.Text == btn_str & button3.Text == btn_str))
+                {               // all possible win conditions
+                    switch (btn_str)
+                    { 
+                        case "o" :
+                            txtBox_main.Text = (btn_str + " wins!");
+                            o_score += 1;
+                            break;
+
+                        case "x":
+                            txtBox_main.Text = (btn_str + " wins!");
+                            x_score += 1;
+                            break;              // incriment score and display winner
+                    }
+
+                    lbl_o_score.Text = "score o: " + o_score;
+                    lbl_x_score.Text = "score x: " + x_score;               // update scoreboard
+
+                    play = false;               // disables buttons until Begin|Rematch are selected
+
+                }  
+            }
+            
+        }
+
+
+        void reset_btns()               // resets buttons to "-" neutral state
+        {
+            List<Button> btns = new List<Button>();             // list of playable buttons (1-9)
+            btns.Add(button1);
+            btns.Add(button2);
+            btns.Add(button3);
+            btns.Add(button4);
+            btns.Add(button5);
+            btns.Add(button6);
+            btns.Add(button7);
+            btns.Add(button8);
+            btns.Add(button9);
+
+            foreach (Button i in btns)
+            {
+                i.Text = "-";
+            }
+
+            if (play)               // if buttons are active then clear display
+            {
+                txtBox_main.Clear();
+            }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)              // playable button method
+        {
+            if ((button1.Text == "-") & (play))             // if buttons are active and current button is neutral
+            {
+                button1.Text = set();               // change button text depending on _flag
+            }
+            check(button1.Text);                // pass current button value to check for win scenario
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((button2.Text == "-") & (play))
+            {
+                button2.Text = set();
+            }
+            check(button2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if ((button3.Text == "-") & (play))
+            {
+                button3.Text = set();
+            }
+            check(button3.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if ((button4.Text == "-") & (play))
+            {
+                button4.Text = set();
+            }
+            check(button4.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if ((button5.Text == "-") & (play))
+            {
+                button5.Text = set();
+            }
+            check(button5.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if ((button6.Text == "-") & (play))
+            {
+                button6.Text = set();
+            }
+            check(button6.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if ((button7.Text == "-") & (play))
+            {
+                button7.Text = set();
+            }
+            check(button7.Text);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if ((button8.Text == "-") & (play))
+            {
+                button8.Text = set();
+            }
+            check(button8.Text);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if ((button9.Text == "-") & (play))
+            {
+                button9.Text = set();
+            }
+            check(button9.Text);
+        }
+
+        private void button_begin_Click(object sender, EventArgs e)
+        {
+            begin = true;
+            play = true;
+            reset_btns();
+
+            o_score = 0;
+            x_score = 0;
+            lbl_o_score.Text = "score o: " + o_score;
+            lbl_x_score.Text = "score x: " + x_score;               // reset all necessary values for fresh game
+
+
+            Random rnd = new Random();
+            int play_first = rnd.Next(0, 2);
+
+            if (play_first == 0)
+            {
+                txtBox_main.Text = "o plays first!";
+                _flag = "o";
+            }
+            else
+            {
+                txtBox_main.Text = "x plays first!";
+                _flag = "x";                // random number gen to see who plays first
+            }
+
+        }
+
+        private void button_rematch_Click(object sender, EventArgs e)
+        {
+            if (begin)
+            {
+                reset_btns();
+                play = true;                // reset certain values excluding score etc
+
+                if (_flag == "o")
+                {
+                    txtBox_main.Text = "o plays first!";
+                }
+                else
+                {
+                    txtBox_main.Text = "x plays first!";                // inverse of last flag(game winner) goes first next game
+                }
+            }
+
+
+        }
+    }
+}
